91337b1247a15cad4d987a654be8ba5a0e781249
2boom <97739181+2boom-ua@users.noreply.github.com>
2026-09-08 04:39:33 +0300

    Adjust luminance threshold for black color detection

--- content.js
+++ content.js
@@ -588,8 +588,9 @@ function isColorWhiteOrBlack(color) {
     // Calculate relative luminance
     const luminance = (0.299 * r + 0.587 * g + 0.114 * b) / 255;
     
-    // White: luminance > 0.9, Black: luminance < 0.1
-    return luminance > 0.9 || luminance < 0.1;
+    // White: luminance > 0.9
+    // Black: luminance < 0.15 (increased from 0.1 to catch near-black colors)
+    return luminance > 0.9 || luminance < 0.15;
     
   } catch (e) {
     return false;
